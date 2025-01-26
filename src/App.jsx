5a741021c634f9083ccdbd6999b43b8f31e3e3cd
Manoj <rayimanoj8@gmail.com>
2025-01-26 14:06:19 +0530

    hehehe

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+    return <>
+      <h1 className='text-green-600'>
+        Hello World!
+      </h1>
+    </>
+
+}
+
+export default App
