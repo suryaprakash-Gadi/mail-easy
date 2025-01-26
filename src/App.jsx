d6cc69ab4214293d696a4577e07da4cc0db2fac5
Manoj <rayimanoj8@gmail.com>
2025-01-26 14:26:50 +0530

    added shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
 import { useState } from 'react'
 import './App.css'
+import {Button} from "@/components/ui/button.jsx";
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card.jsx";
+import {Label} from "@/components/ui/label.jsx";
+import {Input} from "@/components/ui/input.jsx";
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "@/components/ui/select.jsx";
 
 function App() {
-    return <>
-      <h1 className='text-green-600'>
-        Hello World!
-      </h1>
-    </>
+    return (
+        <Card className="w-[350px] mx-auto my-4">
+            <CardHeader>
+                <CardTitle>Sample Form</CardTitle>
+                <CardDescription>use this template</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="grid w-full items-center gap-4">
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="name">Name</Label>
+                            <Input id="name" placeholder="Name of your project" value='a template'/>
+                        </div>
+                        <div className="flex flex-col space-y-1.5">
+                            <Label htmlFor="name">Email</Label>
+                            <Select>
+                                <SelectTrigger id="framework">
+                                    <SelectValue placeholder="Select" />
+                                </SelectTrigger>
+                                <SelectContent position="popper">
+                                    <SelectItem value="he">rayimanoj8@gmail.com</SelectItem>
+                                    <SelectItem value="ee">rayimanoj963@gmail.com</SelectItem>
+                                </SelectContent>
+                            </Select>
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+                <Button variant="outline">Cancel</Button>
+                <Button>Deploy</Button>
+            </CardFooter>
+        </Card>
+    )
 
 }
 
